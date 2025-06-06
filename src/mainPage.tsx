e72ac2886b33f8ae1e1a0ec151bf0d964de35801
Martim Bento <martim.souto.bento@gmail.com>
2025-06-06 16:41:55 +0100

    Added sites list to the main page

--- src/mainPage.tsx
+++ src/mainPage.tsx
@@ -1,10 +1,26 @@
 import type { Workstation, Activity, Site, Region} from "./types"
 import { regions, sites, activities, workstations } from "./data"
+import { useState } from "react"
 
 export default function MainPage() {
+    const [isSiteClicked, setIsSiteClicked] = useState(false)
+    const[clickedSiteName, setClickedSiteName] = useState('')
+
+    const handleClick = (clickedSiteName: any) => {
+        setIsSiteClicked(true)
+        setClickedSiteName(clickedSiteName)
+
+    }
     return (
         <>
             <h1>Welcome!!!</h1>
+            <h2>{clickedSiteName}</h2>
+            {sites.map((site) => {
+                return <ul>
+                            <li onClick={() => {handleClick(site.name)}} key={site.name}>{site.name}</li>
+
+                       </ul>
+            })}
         </>
     )
 }
