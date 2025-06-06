3a52068c04bafa049e0919226a6911a58a36b710
Martim Bento <martim.souto.bento@gmail.com>
2025-06-06 16:29:52 +0100

    Starting work on rendering the sites

--- src/mainPage.tsx
+++ src/mainPage.tsx
@@ -1,5 +1,10 @@
 import type { Workstation, Activity, Site, Region} from "./types"
+import { regions, sites, activities, workstations } from "./data"
 
-export default function mainPage() {
-    
+export default function MainPage() {
+    return (
+        <>
+            <h1>Welcome!!!</h1>
+        </>
+    )
 }
